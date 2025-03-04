ba0519055ad0683579f982676e0e5afeb1ecd2ca
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-03-04 13:19:50 +0530

    update the comments

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import { authOptions } from "@/lib/nextAuthOptions"; // Import your custom NextAuth configuration
 import NextAuth from "next-auth"; // Import NextAuth
 
-// Create the NextAuth handler using your custom configuration
+// Create the NextAuth handler using your custom configuration of authOptions
 const handler = NextAuth(authOptions);
 
 // Export the handler as both GET and POST to handle authentication requests
