779f063e2bc8de4cfa84c48f41fbeeb1160577f4
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-03-04 13:21:27 +0530

    added the username in interface and in userSchema

--- models/User.ts
+++ models/User.ts
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 
 // define type of user schema by using interface  keyword
 export interface User {
+  username: string;
   email: string;
   password: string;
   _id?: mongoose.Types.ObjectId; //defalt way to write _id in mongodb
@@ -14,23 +15,23 @@ export interface User {
 
 const userSchema = new Schema<User>(
   {
+    username: { type: String, required: true },
+
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
   { timestamps: true } // for created-at and updated-at field
 );
 
-// adding pre hook to hash password before saving to database using bcrypt 
+// adding pre hook to hash password before saving to database using bcrypt
 
 userSchema.pre("save", async function (next) {
-
   // if password is not modified then skip this hook and if password is modified then hash the password
 
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 10);
   }
   next();
-
 });
 
 //if user model already exists then use it otherwise create the new model using userSchema
