a0680f92689d951675a10f003250c2537cc1f527
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-03-04 11:11:46 +0530

    update the commments

--- models/User.ts
+++ models/User.ts
@@ -1,7 +1,7 @@
 import mongoose, { Schema, model, models } from "mongoose";
 import bcrypt from "bcryptjs";
 
-// define type of user schema by using interfacee keyword
+// define type of user schema by using interface  keyword
 export interface User {
   email: string;
   password: string;
@@ -20,14 +20,29 @@ const userSchema = new Schema<User>(
   { timestamps: true } // for created-at and updated-at field
 );
 
+// adding pre hook to hash password before saving to database using bcrypt 
+
 userSchema.pre("save", async function (next) {
+
+  // if password is not modified then skip this hook and if password is modified then hash the password
+
   if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 10);
   }
   next();
+
 });
+
+//if user model already exists then use it otherwise create the new model using userSchema
 
 const User = models?.User || model<User>("User", userSchema);
 
 export default User;
 
+/* 
+NOTES ;
+Model:- use to cretae the model of the schema
+
+Models:- An array containing all models associated with this Mongoose instance.
+
+*/
